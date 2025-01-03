fa4c2d5a6d9106e6b6ee9fb2f3ed4e0193b0bae8
Himanshu <hsahu615@gmail.com>
2025-01-03 22:20:12 +0530

    removed AuthProvider.ts file

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -17,7 +17,7 @@ const Login = () => {
       signIn(email, password)
         .then((response) => {
           setShowSpinner(false);
-          login(response.data.data);
+          login(response.data);
         })
         .catch((err) => {
           setShowSpinner(false);
